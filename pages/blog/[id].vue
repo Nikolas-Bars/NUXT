3be214bcdf378adc05180718853f1e1e23fc3ffa
Nikolas-Bars~ <kasp2409@mail.ru>
2024-10-28 12:55:34 +0300

    new project

---
+++ pages/blog/[id].vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>MY BLOG</h1>
+    <div> id is {{route.params.id}}</div>
+  </div>
+</template>
+<script setup>
+  const route = useRoute()
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>
+      All blogs.
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
