9b8a86fe55665b08c8b3dd3741f33956906d2347
Nikolas-Bars~ <kasp2409@mail.ru>
2024-12-15 16:08:51 +0300

    slug lesson 32

--- components/content/blog-posts.vue
+++ components/content/blog-posts.vue
@@ -1,8 +1,15 @@
 <template>
-  <section class="prose">
+  <section class="not-prose font-mono">
+    <div class="column p-4 text-gray-400 text-sm">
+      <div>date</div>
+      <div>title</div>
+    </div>
     <ul>
       <li v-for="post in posts" :key="post._path">
-        <NuxtLink :to="post._path" class="dark:text-white">{{post.title}}</NuxtLink>
+        <NuxtLink :to="post._path" class="column p-4 rounded-md dark:text-white hover:bg-gray-100 dark:hover:text-black">
+          <div class="text-gray-500">2023</div>
+          <div>{{post.title}}</div>
+        </NuxtLink>
       </li>
     </ul>
   </section>
@@ -18,3 +25,8 @@ const {data: posts} = await useAsyncData(
 )
 console.log(posts, 'result')
 </script>
+<style scoped>
+.column {
+  @apply flex items-center space-x-8 py-2 border-b border-gray-200 dark:border-gray-700
+}
+</style>
